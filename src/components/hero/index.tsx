9ba9ed5670f99a1454cd8cd8364ce22a65a21283
Browny <codingbrowny@gmail.com>
2023-06-22 06:49:23 +0000

    hero

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '..'
 
 const Hero = () => {
   return (
-      <header className='hero-wrapper w-screen h-screen overflow-hidden'>
+      <header className='hero-wrapper w-full h-screen overflow-hidden'>
           <Navbar />
     </header>
   )
