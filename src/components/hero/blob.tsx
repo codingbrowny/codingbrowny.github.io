1c9231ad019f39826dee80191254978b9ce53714
Browny <codingbrowny@gmail.com>
2023-07-07 07:23:06 +0000

    removed image from blob

--- src/components/hero/blob.tsx
+++ src/components/hero/blob.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import Profile from "../../../public/images/profiles.png"
 
 const HeroBlob = () => {
   return (
@@ -25,8 +24,7 @@ const HeroBlob = () => {
         129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
         -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
         />
-        {/* @ts-ignore */}
-        <img src="/images/profiles.jpg" alt='Profile' x="12" y="18" />
+        {/* <img src="/images/profiles.jpg" alt='Profile' x="12" y="18" /> */}
       </g>
     </svg>
   );
