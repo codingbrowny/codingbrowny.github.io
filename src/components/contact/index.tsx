9b8b278b56c40c773fdb270db357207c00703ba5
Browny <codingbrowny@gmail.com>
2023-07-09 05:27:39 +0000

    added icons for contact info

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -2,39 +2,54 @@
 import Link from "next/link";
 import * as React from "react";
 import { VscSend } from "react-icons/vsc";
+import { FiPhone, FiMail } from "react-icons/fi";
+import {SlLocationPin} from "react-icons/sl"
 
 const Contact = () => {
   return (
     <div className="flex flex-col sm:flex-row justify-center gap-10 xl:gap-14 w-full md:w-4/5 lg:w-4/6 xl:w-3/5 md:mx-auto">
-      <div className="w-full sm:w-fit space-y-5">
-        <div className="address-group space-y-2">
-          <h4 className="text-primary-text">Phone</h4>
-          <Link
-            href={"tel:+233 556 08 7757"}
-            className="text-primary-lightText"
-          >
-            +233 556 08 7757
-          </Link>
+      <div className="w-full sm:w-fit space-y-7">
+        <div className="address-group flex items-center gap-5">
+          <FiPhone className="text-3xl text-primary-text" />
+          <div className="space-y-2">
+            <header>
+              <h4 className="text-primary-text">Phone</h4>
+            </header>
+            <Link
+              href={"tel:+233 556 08 7757"}
+              className="text-primary-lightText"
+            >
+              +233 556 08 7757
+            </Link>
+          </div>
         </div>
-        <div className="address-group space-y-2">
-          <h4 className="text-primary-text">Email</h4>
-          <Link
-            href={"mailto:codingbrowny@gmail.com"}
-            className="text-primary-lightText"
-          >
-            codingbrowny@gmail.com
-          </Link>
+        <div className="address-group flex items-center gap-5">
+          <FiMail className="text-3xl text-primary-text" />
+          <div className="space-y-2">
+            <header>
+              <h4 className="text-primary-text">Email</h4>
+            </header>
+            <Link
+              href={"mailto:myemail@gmail.com"}
+              className="text-primary-lightText"
+            >
+              myemail@gmail.com
+            </Link>
+          </div>
         </div>
-        <div className="address-group space-y-2">
-          <h4 className="text-primary-text">Location</h4>
-          <span className="text-primary-lightText">
-            Accra Ghana
-          </span>
+        <div className="address-group flex items-center gap-5">
+          <SlLocationPin className="text-3xl text-primary-text" />
+          <div className="space-y-2">
+            <header>
+              <h4 className="text-primary-text">Location</h4>
+            </header>
+            <span className="text-primary-lightText">Accra Ghana</span>
+          </div>
         </div>
       </div>
       <form className="space-y-5 w-full lg:w-4/5">
         <div className="form-group bg-primary-input py-3 px-4 rounded-lg">
-          <label htmlFor="name" className="text-sm text-primary-title">
+          <label htmlFor="name" className="text-sm text-primary-text">
             Name
           </label>
           <input
@@ -46,7 +61,7 @@ const Contact = () => {
           />
         </div>
         <div className="form-group bg-primary-input py-3 px-4 rounded-lg">
-          <label htmlFor="email" className="text-sm text-primary-title">
+          <label htmlFor="email" className="text-sm text-primary-text">
             Email
           </label>
           <input
@@ -58,7 +73,7 @@ const Contact = () => {
           />
         </div>
         <div className="form-group bg-primary-input py-3 px-4 rounded-lg">
-          <label htmlFor="subject" className="text-sm text-primary-title">
+          <label htmlFor="subject" className="text-sm text-primary-text">
             Subject
           </label>
           <input
@@ -70,7 +85,7 @@ const Contact = () => {
           />
         </div>
         <div className="form-group bg-primary-input py-3 px-4 rounded-lg">
-          <label htmlFor="message" className="text-sm text-primary-title">
+          <label htmlFor="message" className="text-sm text-primary-text">
             Message
           </label>
           <textarea
