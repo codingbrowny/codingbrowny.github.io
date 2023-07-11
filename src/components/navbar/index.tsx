66e478cd2ec4a87c3c3ed55f43f949654909f97f
Browny <codingbrowny@gmail.com>
2023-07-11 14:23:04 +0000

    hidden navbar style

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -12,11 +12,12 @@ const Navbar = () => {
       id="navbar"
       className="sticky top-0 z-[9999] transition-all duration-300 w-full"
     >
-      <nav className="container py-5 bg-white flex items-center justify-between">
+      <nav className="container py-5 bg-primary-body flex items-center justify-between">
         <h1 className="logo-text text-xl sm:text-2xl font-semibold text-primary-title">
           Browny
         </h1>
         <button
+          type="button"
           id="nav-toggle-btn"
           role="button"
           className="md:hidden rounded-full"
@@ -26,10 +27,7 @@ const Navbar = () => {
 
         <ul
           id="nav-list"
-          className="shadow-md md:shadow-none px-[1rem] sm:px-[2rem] md:px-0 
-          absolute left-0 top-[-400px] space-y-5 md:space-y-0 py-5 pb-8 md:py-0 md:pb-0 w-full 
-          md:w-auto md:static md:z-auto md:flex md:items-center md:gap-8 lg:gap-10 transition-all 
-          ease-in md:opacity-100 duration-300 bg-container md:bg-[inherit]"
+          className="shadow-md md:shadow-none px-[1rem] sm:px-[2rem] md:px-0 absolute left-0 -top-96 space-y-5 md:space-y-0 py-5 pb-8 md:py-0 md:pb-0 w-full md:w-auto md:static -z-[1] md:z-auto md:flex md:items-center md:gap-8 lg:gap-10 transition-all md:opacity-100 duration-300 bg-container md:bg-[inherit]"
         >
           <li className="nav-list-item font-medium text-primary-text hover:text-primary">
             <Link href={"#hero"}>Home</Link>
@@ -83,8 +81,6 @@ const Navbar = () => {
         </ul>
       </nav>
     </header>
-
-    // </header>
   );
 };
 
